8fc09e44f0aa94537bc4f6223c08db09d8ff2848
KrisztanZero <s.molnar.krisztian@gmail.com>
2024-04-01 21:06:15 +0200

    Initial commit

---
+++ src/main/java/org/chronopm/chronopmspringapi/ChronoPmSpringApiApplication.java
@@ -0,0 +1,13 @@
+package org.chronopm.chronopmspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoPmSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChronoPmSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chronopm/chronopmspringapi/ChronoPmSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chronopm.chronopmspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoPmSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
